b7b22127d2b5ec0d1123bd9561b47db42a92b7a0
RachitjPatel <rachitjp2935@gmail.com>
2025-02-09 14:06:39 -0500

    created main  clase and updted the code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
 ﻿namespace Assignment3_classes
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
 }
--- main.cs
+++ main.cs
@@ -1,7 +1,17 @@
 ﻿
 namespace Assignment3_classes
-{
-    internal class main
+{ 
+    // FILE: Main.cs
+    // DESCRIPTION: Entry point of the program that starts execution.
+
+    internal class MainProgram
     {
+        static void Main(string[] args)
+        {
+            // Create an object of the Run class
+            Run programRun = new Run();
+            // Call the method to start the program logic
+            programRun.StartTheCode();
+        }
     }
 }
